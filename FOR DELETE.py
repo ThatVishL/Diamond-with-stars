106d2b54d755e6babc28bc08d76176a9c42653c4
ThatVishL <113586539+ThatVishL@users.noreply.github.com>
2022-10-12 15:52:15 +0300

    Add files via upload

---
+++ FOR DELETE.py
@@ -0,0 +1,26 @@
+            #STAR DIAMOND
+#Write a function that prints out a diamond of stars, length 7
+def star_diamond():
+    for row in range(4):
+        for space in range(3-row):
+            print(' ',end='')
+        for star in range(row+1):
+            print('*',end='')
+        for star in range(row):
+            print('*',end='')
+        print()
+    for row in range(3):
+        for space in range(row+1):
+            print(' ', end='')
+        for star in range(3-row):
+            print('*', end='')
+        for star in range(2-row):
+            print('*', end='')
+        print()
+star_diamond()
+
+
+
+
+
+
